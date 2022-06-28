2c5c11c5dafef55d7d5746c7d4fd8bf0decd816a
test <test@naver.com>
2022-06-29 01:16:41 +0900

    svg bg added

--- src/pages/Analysis.jsx
+++ src/pages/Analysis.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Stacked, Pie, Button, SparkLine} from '../components';
-import { earningData, SparklineAreaData, ecomPieChartData } from '../data/dummy';
+import { earningData, SparklineAreaData, ecomPieChartData, prismbg } from '../data/dummy';
 import {useStateContext} from '../contexts/ContextProvider'
 import { MdOutlineSupervisorAccount } from 'react-icons/md';
 import { useParams } from 'react-router-dom';
@@ -10,7 +10,7 @@ const Analysis = () => {
     {/*http://localhost:3000/Analysis/PCS_PP/123*/}
     const {product, commitid} = useParams('product', 'commitid');
     return (
-        <div className="mt-12">
+        <div className="mt-12 bg-neutral-200">
             <div className="flex flex-wrap justify-center ">
                 <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-64 
                 rounded-xl w-full p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
@@ -35,6 +35,14 @@ const Analysis = () => {
                 <div className="flex m-3 flex-wrap justify-center gap-4 items-center">
                     <div key={'sam'} className="bg-white h-44 dark:text-gray-200 
                                 dark:bg-secondary-dark-bg md:w-56  p-4 pt-9 rounded-2xl ">
+                        {/*https://flowbite.com/docs/components/spinner/*/}
+                        <button disabled type="button" class="py-2.5 px-5 mr-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center">
+                            <svg role="status" class="inline w-4 h-4 mr-2 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#1C64F2"/>
+                            </svg>
+                            Loading...
+                        </button>
                         <button
                         type="button"
                         style={{ color: '#03C9D7', backgroundColor: '#E5FAFB' }}
